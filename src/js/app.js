8bc1e434dbdc4566b3c4a6c070f0b7b54ebad1cc
maks_chaliy <mrgoodchaliy@yandex.ru>
2022-10-25 00:41:02 +0300

    add header + hero

--- src/js/app.js
+++ src/js/app.js
@@ -12,3 +12,36 @@ flsFunctions.isWebp();
 //=============/=============/=============/=============/=============
 //========== READYCODES ==========
 //import '../js/readyCodes/    ';
+
+
+
+
+// //прокрутка при клике
+// const $menuBody = document.querySelector('.nav');
+// const $menuLinks = document.querySelectorAll('.nav__link[data-goto]');
+
+// if($menuLinks.length > 0) {
+// 	$menuLinks.forEach(menuLink =>{
+// 		menuLink.addEventListener('click', onMenuLinkClick);
+// 	})
+
+// 	function onMenuLinkClick(e) {
+// 		const menuLink = e.target;
+// 		if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+// 			const gotoBlock = document.querySelector(menuLink.dataset.goto)
+// 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+// 			if($menuIcon.classList.contains('_active')) {
+// 				document.body.classList.remove('_stop-scroll')
+// 				$menuIcon.classList.remove('_active');
+// 				$menuBody.classList.remove('_active');
+// 			}
+
+// 			window.scrollTo({
+// 				top:gotoBlockValue,
+// 				behavior: 'smooth'
+// 			});
+// 			e.preventDefault();
+// 		}
+// 	}
+// }
