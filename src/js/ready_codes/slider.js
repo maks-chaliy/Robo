05b9b434bc0ce99fbe8d952e2d7f56384d6b611a
maks_chaliy <mrgoodchaliy@yandex.ru>
2022-10-31 02:50:06 +0300

    add tariffs

--- src/js/ready_codes/slider.js
+++ src/js/ready_codes/slider.js
@@ -1,106 +1,24 @@
-const $trainersSlider = document.querySelector('.trainers__slider');
+'use strict';
 
-let trainersSlider = new Swiper($trainersSlider, {
+const $slider = document.querySelector('.slider');
 
-	slidesPerView: 4,
-	spaceBetween: 40,
-	// breakpoints: {
-	// 	769: {
-	// 		slidesPerView: 4,
-	// 		spaceBetween: 40,
-	// 	}
-	// },
-	scrollbar: {
-		el: '.trainers__scroll',
-		dragable: true,
-	},
-		navigation: {
-		nextEl: '.trainers__slider-btn--next',
-		prevEl: '.trainers__slider-btn--prev',
-	},
-})
-
-
-
-/*
-// const $sliderMain = document.querySelector('.slider-main');
-
-// let sliderMain = new Swiper($sliderMain, {
-// 	slideClass: 'slider-main__slide',
-// 	wrapperClass: 'slider-main__wrapper',
-// }); 
-
-if (document.querySelector('.slider-main')) {
-	new Swiper('.slider-main', {
-
-		//=============/=============/=============/=============/=============
-		//переименовать классы слайдера
-		slideClass: 'slider-main__slide',
-		wrapperClass: 'slider-main__wrapper',
-
-		//=============/=============/=============/=============/=============
-		//количество слайдов в одном блоке
+if($slider) {
+	new Swiper(".slider", {
+		slideClass: 'slider__slide',
+		wrapperClass: 'slider__wrapper',
 		slidesPerView: 4,
-
-		//=============/=============/=============/=============/=============
-		//бесконечный цикл прокрутки по кругу
-		loop: true,
-
-		//=============/=============/=============/=============/=============
-		//отступ между слайдами
 		spaceBetween: 40,
+		//parallax: true,
+		speed: 1500,
 
-		//=============/=============/=============/=============/=============
-		//скорость
-		// speed: 1400,
-
-		//=============/=============/=============/=============/=============
-		//эффект паралакса
-		// parallax: true,
-
-		//=============/=============/=============/=============/=============
-		//сменяет слайды группой
-		// slidesPerGroup: 2,
-
-		//=============/=============/=============/=============/=============
-		//arrows
 		navigation: {
-			nextEl: '.slider-main__arrow-next',
-			prevEl: '.slider-main__arrow-prev',
+			nextEl: '.slider__btn.slider__btn--next',
+			prevEl: '.slider__btn.slider__btn--prev',
 		},
 
-		//=============/=============/=============/=============/=============
-		//dotts
-		// pagination: {
-		// 	el: '.slider-main__dotts',
-		// 	type: 'bullets',
-		// 	//если нужны цифры нужен другой тип пагинации
-		// 	//type: 'fraction',
-		// 	//чтобы точки были кликабельны
-		// 	clickable: true,
-		// },
-
-		//=============/=============/=============/=============/=============
-		//автоматическое переключение слайдера каждые 2 сек
-		// autoplay: {
-		// 	delay: 4000,
-		// },
-
-		//=============/=============/=============/=============/=============
-		// breakpoints: {
-		// 	320: {
-		// 		slidesPerView: 1,
-		// 		spaceBetween: 10
-		// 	},
-		// 	768: {
-		// 		slidesPerView: 2,
-		// 		spaceBetween: 20
-		// 	},
-		// 	1024: {
-		// 		slidesPerView: 3,
-		// 		spaceBetween: 30
-		// 	}
-		// },
-	});
-}
-*/
+		scrollbar: {
+			el: '.slider__scroll',
+			draggable: true,
+		},
+	})
+};
